6df314a8bcfc6604c35d95fd3a1d0cc32af0c9b2
William Fichtner <william_25_35@hotmail.com>
2017-02-09 11:50:52 -0700

    chuggin along

--- CMPE1600_ICA7/Form1.cs
+++ CMPE1600_ICA7/Form1.cs
@@ -6,15 +6,91 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GDIDrawer;
 using System.Windows.Forms;
 
 namespace CMPE1600_ICA7
 {
     public partial class Form1 : Form
     {
+        CDrawer canvas = new CDrawer(600, 600);
+        int xCoor = 15;
+        int yCoor = 15;
+        bool red = true;
+        bool blue = false;
+        bool green = false;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            canvas.Scale = 20;
+            canvas.AddCenteredEllipse(15, 15, 1, 1, Color.Red);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                    UpPress();
+                    break;
+                case Keys.Left:
+                    LeftPress();
+                    break;
+                case Keys.Right:
+                    RightPress();
+                    break;
+                case Keys.Down:
+                    DownPress();
+                    break;
+                case Keys.B:
+                    BlueBall();
+                    break;
+                case Keys.R:
+                    break;
+                case Keys.G:
+                    break;
+                case Keys.X:
+                    break;
+                case Keys.Shift:
+                    break;
+                case Keys.Alt:
+                    break;
+                case Keys.Control:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void UpPress()
+        {
+            yCoor -= 1;
+            canvas.AddCenteredEllipse(xCoor, yCoor, 1, 1, Color.Red);
+        }
+        public void DownPress()
+        {
+            yCoor += 1;
+            canvas.AddCenteredEllipse(xCoor, yCoor, 1, 1, Color.Red);
+        }
+        public void LeftPress()
+        {
+            xCoor -= 1;
+            canvas.AddCenteredEllipse(xCoor, yCoor, 1, 1, Color.Red);
+        }
+        public void RightPress()
+        {
+            xCoor += 1;
+            canvas.AddCenteredEllipse(xCoor, yCoor, 1, 1, Color.Red);
+        }
+        public void BlueBall()
+        {
+
         }
     }
 }
